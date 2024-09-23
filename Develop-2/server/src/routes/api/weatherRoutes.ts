f379e9c030092c4ec4148315ac83f9e2edee1a78
Ryan Fredericks <Kurt@stephanies-mbp.myfiosgateway.com>
2024-09-23 00:38:16 -0400

    Pushing new code for server files

--- Develop-2/server/src/routes/api/weatherRoutes.ts
+++ Develop-2/server/src/routes/api/weatherRoutes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import weatherService from '../../service/weatherService';
+import historyService from '../../service/historyService';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
@@ -8,10 +10,27 @@ const router = Router();
 router.post('/', (req, res) => {
   // TODO: GET weather data from city name
   // TODO: save city to search history
+  const {city} = req.body;
+  if(!city) {
+    return res.sttaus(400).json({ error: `City name is required`});
+  }
+  const weatherData = await weatherService.getWeather(city);
+
+  await historyService.addCityToHistory(city);
+
+
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (req, res) => {
+try{
+  const history = await historyService.getHistory();
+  return resizeBy.json(history);
+} catch(error){
+    return resizeBy.status(500).json({error: `Error fetching search history`});
+  }
+}
+);
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req, res) => {});
--- Develop-2/server/src/service/historyService.ts
+++ Develop-2/server/src/service/historyService.ts
@@ -1,17 +1,43 @@
-// TODO: Define a City class with name and id properties
+import {cities as fs} from 'fs';
+import path from 'path';
 
+// TODO: Define a City class with name and id properties
+class City {
+  constructor(public id: string, public name: string) {}
+}
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+constructor () {
+  this.filePath = path.join(__dirname, 'searchHistory.json');
+}
+private async read() {}
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+private async write(cities: City[]) {
+try {
+  const data = await FileSystem.writeFile(this.filePath, JSON.stringify(cities, null, 2), 'utf-8');
+  return JSON.parse(data);
+} catch(error){
+    return [];
+  }
+}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    return this.read();
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {
+    const cities = await this.getCities(); 
+    const newCity = new City(Date.now().toString(), name);
+    cities = cities.filter(city => city.id !== id);
+    await this.write(cities);
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  async removeCity(id: string) {
+    let cities = await this.getCities();
+    cities = cities.filter(city => city.id !==id);
+    await this.write(cities);
+  }
 }
 
 export default new HistoryService();
